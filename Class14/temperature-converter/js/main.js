fe9669f6fac36939836ca4c460224b037050236d
BlindBy <blindnightshade@hotmail.com>
2022-11-26 23:30:52 -0700

    temp converter completed, as well as other tasks. - Class 14 complete

--- Class14/temperature-converter/js/main.js
+++ Class14/temperature-converter/js/main.js
@@ -1,1 +1,18 @@
 //Write your pseduo code first! 
+
+/*
+
+
+get a value to convert
+convert the number to the desired unit
+display converted temp
+
+*/
+
+document.querySelector(".enter").addEventListener('click', tempConvert)
+
+function tempConvert(){
+    let temp = document.querySelector(".temp").value
+    temp = (temp * 9/5) + 35
+    document.querySelector(".conversion").innerText = temp
+}
