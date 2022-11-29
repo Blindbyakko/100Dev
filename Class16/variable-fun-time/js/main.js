3639d0440075a524f545f18a398c27b21da0f251
BlindBy <blindnightshade@hotmail.com>
2022-11-29 14:59:50 -0700

    Value-fun-time task complete - class 16

--- Class16/variable-fun-time/js/main.js
+++ Class16/variable-fun-time/js/main.js
@@ -1,18 +1,35 @@
 //--- Easy
 //create a variable and assign it a number
+let number = 1;
 
 //minus 10 from that number
 
+number = number - 10;
 //print that number to the console
 
+console.log(number) // should be -9
 //--- Medium
 //create a variable that holds a value from the input
 
+let inputVal = document.querySelector('#danceDanceRevolution').value;
+
 //add 25 to that number
 
+inputVal += 25
+
 //alert that number
+
+alert(inputVal)
 
 //--- Hard
 //create a variable that holds the h1
 
+let h1val = document.querySelector('h1')
+
 //add an event listener to that element that console logs the sum of the two previous variables
+
+h1val.addEventListener('click', sum)
+
+function sum(){
+    console.log(number + parseInt(inputVal))
+}
