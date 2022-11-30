415c93df386768adcc8d933be2fbd4a3715467d7
BlindBy <blindnightshade@hotmail.com>
2022-11-30 04:16:14 -0700

    Stop snacking task completed - class 16 - loop practice

--- Class16/stop-snacking/js/main.js
+++ Class16/stop-snacking/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+
+function stopSnacking(){
+    let snacks = document.querySelector('input').value
+    while(snacks > 0){
+        console.log("stop!")
+        snacks--
+    }
+}
+
+document.querySelector("#help").addEventListener('click', stopSnacking)
