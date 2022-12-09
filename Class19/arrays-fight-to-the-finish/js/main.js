a35ad6f705e927d7e1d24d84fb99f826a0f7a0f9
BlindBy <blindnightshade@hotmail.com>
2022-12-09 00:31:52 -0700

    Class 19: Arrays fight to the finish task

--- Class19/arrays-fight-to-the-finish/js/main.js
+++ Class19/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,20 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
+let movieTitles = ["alien", "alien2", "alien 3"]
+
+for(let i = 0; i < movieTitles.length; i++){
+    document.getElementsByTagName("h2")[0].innerText  += movieTitles[i] + " ";
+    console.log(movieTitles[i])
+}
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 
-
+let numbers = [1,2,3,4,5]
+numbers.forEach((value,index)=> numbers[index] = value + 3)
+console.log(numbers)
 //Find the average of all the numbers from question three
+
+let sum = numbers.reduce((value,sum) => value + sum)
+let average = sum/numbers.length
+console.log(sum)
+console.log(average)
