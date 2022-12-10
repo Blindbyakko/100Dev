733bff027bcf55c76a206c49739077398761c8a1
BlindBy <blindnightshade@hotmail.com>
2022-12-10 04:50:15 -0700

    Class20 - review in paradise pater paradise task

--- Class20/review-in-paradise-after-paradise/js/main.js
+++ Class20/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,20 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+
+function hiByeWeClosed(arr){
+        console.log(arr[arr.length-1])
+        if(arr[0] < arr[arr.length-1]){
+            alert("Hi")
+        }
+        else if(arr[0] > arr[arr.length-1]){
+            alert("Bye")
+        }
+        else{
+            alert("Hi")
+            alert("We Close in an hour")
+        }
+}
+
+hiByeWeClosed([2,1,2,2])  // equal, trigger else
+hiByeWeClosed([2,1,2,1])  // greater, trigger else if
+hiByeWeClosed([2,1,2,3])  // less then, trigger if
