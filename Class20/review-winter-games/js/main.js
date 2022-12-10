a6ff5649869c3c78cb503ea6d6aa9adc4bc8cea0
BlindBy <blindnightshade@hotmail.com>
2022-12-10 06:09:13 -0700

    Class20 review winter games task   Class complete

--- Class20/review-winter-games/js/main.js
+++ Class20/review-winter-games/js/main.js
@@ -1,1 +1,23 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+
+const grabEvenNumbers = (arr) => {
+    let evenArr  = []
+    // For loop method
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i]%2 == 0){
+    //         evenArr.push(arr[i])
+    //     }
+    // }
+
+    //for each method
+    arr.forEach((element,i) => {
+        if(element%2 == 0){
+            evenArr.push(arr[i])
+        }
+    });
+
+    return evenArr
+}
+
+console.log(grabEvenNumbers([1,2,3,4,5,6,7,8,9,10]))
