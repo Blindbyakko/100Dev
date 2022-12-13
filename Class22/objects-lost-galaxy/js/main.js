eb9ff370063446e97e464a9de86b3912c7aa6222
BlindBy <blindnightshade@hotmail.com>
2022-12-13 02:09:39 -0700

    class 22 obj street fighter task

--- Class22/objects-lost-galaxy/js/main.js
+++ Class22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,91 @@
 //Create a mouse object that has four properties and three methods
+
+
+// let mouse = {
+//     colour: "white",
+//     type: "field",
+//     length: 12,
+//     weight: 21,
+
+//     details(){
+//         console.log(`The ${type} mouse is ${colour} in colour. It is ${length} in length and weights ${weight}g`)
+//     }
+// }
+
+// My mouse obj
+/*
+let mouse = {};
+
+mouse.color = "white";
+mouse.type = "field";
+mouse.length = 12;
+mouse.weight = 21;
+
+mouse.details = function(){
+    console.log(`The ${this.type} mouse is ${this.colour} in colour. It is ${this.length} in length and weights ${this.weight}g`)
+};
+mouse.jump = function(){
+    console.log(`The ${this.type} mouse jumps at you! AHHHH`)
+};
+
+mouse.sound = function(){
+    console.log(`The ${this.type} mouse squeak squeak squeak`)
+}*/
+//==========================
+
+let mouse = {}
+
+mouse.brand = "Logitech"
+mouse.colour = "black";
+mouse.model = "MX Ergo";
+mouse.wireless = true;
+
+mouse.leftClick = function(){
+    console.log("LEFT CLICKKKK")
+}
+mouse.rightClick = function(){
+    console.log("RIGHT CLICKKKK")
+}
+
+mouse.scroll = function() {
+    console.log( "JUMP  JUMP JUMP")
+}
+
+let mouse2 = {}
+
+mouse2.brand = "Logitech"
+mouse2.colour = "black";
+mouse2.model = "Pro Wireless";
+mouse2.wireless = true;
+
+mouse2.leftClick = function(){
+    console.log("LEFT CLICKKKK")
+}
+mouse2.rightClick = function(){
+    console.log("RIGHT CLICKKKK")
+}
+
+mouse2.scroll = function() {
+    console.log( "JUMP  JUMP JUMP")
+}
+
+
+let mouse3 = {}
+
+mouse3.brand = "Apple"
+mouse3.colour = "Silver";
+mouse3.model = "Magic Trackpad 2";
+mouse3.wireless = true;
+
+mouse3.leftClick = function(){
+    console.log("LEFT CLICKKKK")
+}
+mouse3.rightClick = function(){
+    console.log("RIGHT CLICKKKK")
+}
+
+mouse3.scroll = function() {
+    console.log( "SLIDE TWO FINGERS UP")
+}
+
+
--- Class22/objects-street-fighter/js/main.js
+++ Class22/objects-street-fighter/js/main.js
@@ -1,1 +1,24 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name, country, special_ability, hitPoints){
+    this.name = name
+    this.country = country
+    this.special_ability = special_ability
+    this.hitPoints = hitPoints
+
+    this.details = function(){
+        console.log(`You've selcted ${this.name} from ${this.country}. ${this.name}'s has ${this.hitPoints} Hit Points and its Special Ability is: ${this.special_ability}`)
+    }
+    this.special = function(){
+        console.log(`${name} uses ${special_ability}!!!`)
+    }
+    this.punch = function(){
+        console.log(` ${name} uses punch! `)
+    }
+}
+
+let ken = new StreetFighter("ken","USA","Fireball",100)
+
+ken.details()
+ken.special()
+ken.punch()
