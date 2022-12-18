28acb8ab59b7f6aa46178eb4a549d63ecf216de4
BlindBy <blindnightshade@hotmail.com>
2022-12-18 03:18:26 -0700

    Class23 objects - lost galaxy task

--- Class23/objects-lost-galaxy/js/main.js
+++ Class23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,26 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {}
+
+dog.breed = "mutt"
+dog.colour = "black"
+dog.size = "large"
+dog.name = "fiddo"
+
+dog.bark = function(){
+    console.log("BARK BARK BARK! GRRRRR!!!")
+}
+
+dog.jump = function(){
+    console.log("Dog jumps up to greet you!")
+}
+
+dog.eat = function(){
+    console.log("Dog is enjoying it's kibble. Yummmmm")
+}
+
+console.log(`The dog is type ${dog.breed}. It's coat is ${dog.colour}, and it's size is ${dog.size}. It's name is ${dog.name}`)
+
+dog.eat()
+dog.jump()
+dog.bark()
