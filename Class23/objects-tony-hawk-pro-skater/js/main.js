aae2d9b6985ebbbdad58e45a438445ba2610f5cd
BlindBy <blindnightshade@hotmail.com>
2022-12-18 03:35:09 -0700

    Class23 objects tony hawk pro skaker task

--- Class23/objects-tony-hawk-pro-skater/js/main.js
+++ Class23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,26 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that makes skating game characters with 4 properties and 3 methods
+
+
+function CharacterCreater(name, signatureMove, brand, stance){
+    this.name = name
+    this.signatureMove = signatureMove
+    this.brand = brand
+    this.stance = stance
+
+    this.stanceSwitch = function(){
+        console.log(`${this.name} switches stance!`)
+    }
+
+    this.ollie = function(){
+        console.log(`${this.name} does an ollie! AMAZING!`)
+    }
+    this.kickflip = function(){
+        console.log(`${this.name} does an kickflip! OMGGGGGGG!!!!!`)
+    }
+    this.describeCharacter = function(){
+        console.log(`${this.name} signature move is the ${this.signatureMove}. ${this.name} represents the ${this.brand} brand and skates ${this.stance}`)
+    }
+}
+
+ let tony = new CharacterCreater("Tony Hawk", 900, "Birdhouse", "normal")
+ tony.describeCharacter()
