37c69e844fbfa2a9822b67ad966b7da38fef6f60
BlindBy <blindnightshade@hotmail.com>
2022-12-20 05:41:11 -0700

    Fixed my input re ads. need to work on calc obj now

--- A PlayGround/Calculator/js/main.js
+++ A PlayGround/Calculator/js/main.js
@@ -1,12 +1,17 @@
 function Calculator(){
     this.finalResult = 0;
-
-    this.calculate = function(){
-        
+    this.expression = ""
+    this.evaluate = function(){
+        console.log("time to add")
+    }
+    this.display = function(calcInput){
+      console.log("hello display")
+      this.expression = calcInput
+      display.innerText += this.expression
     }
 }
 
-let display = document.querySelector(".display").innerText = "1"
+let display = document.querySelector(".display")
 
 
 // addEventListener('click', (event) => {
@@ -14,22 +19,49 @@ let display = document.querySelector(".display").innerText = "1"
 //    console.log(numerator)
 // });
 
-const numberButtons = document.querySelectorAll(".numberBtn")
-const operator = document.querySelectorAll("operateBtn")
+const numberButtons = document.querySelectorAll(".numberBtn");
+const operator = document.querySelectorAll(".operatorBtn");
+const evaluate = document.querySelector(".equationBtn");
 
+const myCalc = new Calculator()
 
-NumberButtons()
-function NumberButtons(){
-    console.log("hello")
-    console.log(numberButtons.length)
-    numberButtons.forEach((btns) => {
-    // btns.addEventListener("click", (event) => {
-    //   calculator.displayCurrentNumbers(event.target.innerText);
-      
-    // });
+numberButtons.forEach((btns) => {
+  btns.addEventListener("click", (event) => {
+    // display.innerText = btns.innerText
     console.log(btns.innerText)
+    myCalc.display(btns.innerText)
   });
-}
+  // myCalc.display(btns.innerText)
+});
+operator.forEach((btns) => {
+  console.log("hello")
+  btns.addEventListener("click", (event) => {
+    // display.innerText = btns.innerText
+    myCalc.display(btns.innerText)
+    console.log( btns.innerText)
+  });
+});
+// evaluate.forEach((btns) => {
+  evaluate.addEventListener("click", (event) => {
+    myCalc.evaluate(evaluate.innerText)
+    console.log( evaluate.innerText)
+  });
+// });
+
+
+// NumberButtons()
+// function NumberButtons(){
+//     console.log("hello")
+//     // console.log(numberButtons.length)
+//     numberButtons.forEach((btns) => {
+//     btns.addEventListener("click", (event) => {
+//       display.innerText = btns.innerText
+//       console.log(btns.innerText)
+//     });
+
+//     // console.log(btns.innerText)
+//   });
+// }
 
 // console.log(numerator)
 // let numerator = document.querySelectorAll("numberBtn")
