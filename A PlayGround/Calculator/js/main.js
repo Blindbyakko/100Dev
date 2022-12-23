dee4a3cc43785c41cb3afff57f461dbb822df46f
BlindBy <blindnightshade@hotmail.com>
2022-12-22 23:42:56 -0700

    Checkoperator function added

--- A PlayGround/Calculator/js/main.js
+++ A PlayGround/Calculator/js/main.js
@@ -1,11 +1,12 @@
 function Calculator(){
     this.finalResult = 0;
     this.expression = display.innerText
+    this.stack = []
     this.evaluate = function(){
         console.log(display.innerText)
         this.expression = display.innerText
         console.log(this.expression)
-        readExpression(this.expression)
+        readExpression()
         console.log("time to add")
     }
     this.display = function(calcInput){
@@ -13,12 +14,43 @@ function Calculator(){
       this.expression = calcInput
       display.innerText += this.expression
     }
+
+    // Will read in the express and parse out the sign
     function readExpression(){
-      console.log("----------")
-      console.log(expression)
-      console.log("----------")
-      // this.expression = this.expression.split("")
-      // console.log(this.expression)
+      this.expression = display.innerText.split("")
+      this.expression.forEach((value) =>{
+        console.log(typeof value)
+            if(checkOperator(value)){
+              console.log("value is")
+            }
+      })
+    }
+    /* 
+    helper function - takes input from readExpression, and will
+    return the operation
+    */
+    function checkOperator(value){
+      switch(value){
+        case("+"):{
+          return true
+          break;
+        }
+        case("-"):{
+          return true
+          break;
+        }
+        case("x"):{
+          return true
+          break;
+        }
+        case("/"):{
+          return true
+          break;
+        }
+        default:{
+          return false
+        }
+      }
     }
 }
 
